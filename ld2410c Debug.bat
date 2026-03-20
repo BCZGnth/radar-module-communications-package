dd8b52cd7e6ee462d1b8cb310e04f6364a1f6979
Ben <Benjamin.Good.nth@gmail.com>
2026-03-20 11:44:29 -0400

    Deleting the batch scripts (again)

--- ld2410c Debug.bat
+++
@@ -1,4 +0,0 @@
-cd "Python Communication\HLK-LD2410C"
-call conda activate 6Alert
-python ld2410.py
-pause
--- ld2450 Debug.bat
+++
@@ -1,4 +0,0 @@
-cd "Python Communication\HLK-LD2450"
-call conda activate 6Alert
-python print_targets.py
-pause
